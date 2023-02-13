82bd96ebf849c7c5c3aeb3deeb4553387cd61bb9
sauce-picante <tdougherty16@gmail.com>
2023-02-13 11:35:38 -0500

    first

---
+++ scripts.py
@@ -0,0 +1,22 @@
+class scripts():
+
+    def get_train_ids(df,lines):
+        lst = []
+        for line in lines:
+            temp_df = df.loc[(df['line'] == line)]
+            lst.append(temp_df.loc[df_])
+
+    def get_max_delay(df,lines):
+        test_list = []
+        for line in lines:
+            temp_df = df.loc[(df['line'] == line)]
+            test_list.append(temp_df['delay_minutes'].max())
+        return test_list
+    
+    def get_avg_delays(df,lines):
+        lst = []
+        for line in lines:
+            temp_df = df.loc[(df['line'] == line)]
+            lst.append(temp_df['delay_minutes'].mean())
+        return lst
+    
