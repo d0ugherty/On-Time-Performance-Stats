64d94956d50f819bc0a44e0f5fcb034bba57a5e4
thomas <tdougherty16@gmail.com>
2023-04-21 16:06:19 -0400

    distributions and moved CSV formatting

---
+++ csv_funct.py
@@ -0,0 +1,77 @@
+import os
+import pandas as pd
+import numpy as np
+
+class csv():
+
+    ### Removes unused columns from weather dataframe and converts objects to datatypes. 
+    ### Takes an optional month name string to return a dataframe for a single month  
+    def format_weather(current_df, month=""):
+        print("Formatting weather dataframe...")
+        print("Changing datatypes...")
+        new_df = current_df.astype({'STATION':'category',
+                                    'DATE':'datetime64[ns]'})
+        print("Dropping columns...")
+        new_df.drop(labels=['ELEVATION','LATITUDE', 'LONGITUDE', 'MDSF', 'AWND', 'SNWD',
+                            'WESD', 'WESF','WT01', 'WT02', 'WT03',
+                            'WT04', 'WT05',"WT06","WT07","WT08",
+                            "WT09","WT11"], axis=1, inplace=True)
+        if month == "" :
+            print("Done formatting weather dataframe")
+            return new_df
+        else:
+            print("Done formatting weather dataframe")
+            new_df_w_month = new_df.loc[new_df['DATE'].apply(pd.Timestamp.month_name) == month]
+            return new_df_w_month
+    
+    ### clean up services dataframes
+    ### Drop status column bc it's not used and lateness for Amtrak is not tracked
+    ### in the CSV file. Meadowlands is a special service so that is dropped as well.
+    def format_services(current_df):
+        print("Dropping columns...")
+        if 'status' in current_df:
+            current_df.drop(labels=['status'], axis=1, inplace=True)
+        if 'line' in current_df:
+            current_df.drop(current_df[current_df['line'] == 'Meadowlands Rail'].index, inplace=True)
+        if 'type' in current_df:
+            current_df.drop(labels=['type'], inplace=True)
+        if 'stop_sequence' in current_df:
+            current_df.drop(labels=['stop_sequence'], axis=1, inplace=True)
+        if ('from_id' in current_df) & ('to_id' in current_df):
+            current_df.drop(labels=['from_id', 'to_id'], axis=1, inplace=True)
+        print("Changing datatypes...")
+        new_df = current_df.astype({'date' : 'datetime64[ns]',
+                        'train_id' : 'category',
+                        'from' : 'category',
+                        'to' : 'category',
+                        'scheduled_time' : 'datetime64[ns]',
+                        'actual_time' : 'datetime64[ns]',
+                        'delay_minutes' : 'float16',
+                        'line' : 'category'},
+                        errors='ignore')
+        new_df.dropna(how='any', inplace=True)
+        print("Done formatting dataframe")
+        return new_df
+
+    ### combines all CSV files into a single dataframe
+    ### it also exports the dataframe into a combined CSV file
+    ### combines all CSV files into a single dataframe
+    ### it also exports the dataframe into a combined CSV file
+    def combine_csvs(directory):
+        csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
+        dfs = []
+
+        print("Combining CSV files....")
+        for file in csv_files:
+            df = pd.read_csv(os.path.join(directory, file))
+            dfs.append(df)
+        combined_df = pd.concat(dfs, ignore_index=True)
+        print("Formatting new CSV file...")
+        combined_df = csv.format_services(combined_df)
+        # make a new CSV for the dataframe
+        print("Exporting....")
+        compression_opts = dict(method='zip', archive_name='all_services.csv')  
+        combined_df.to_csv('out.zip', index=False, compression=compression_opts)
+
+        print("CSV files successfully combined and exported.")  
+        return combined_df
--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,7 @@
 # Functions that can be called to hopefully make my life easier
 # and make the project more readable
 
-import os
+import scipy.stats as stats
 import pandas as pd
 import numpy as np
 import statistics
@@ -45,56 +45,7 @@ class helper():
             except:
                 print("get delay date error")
         return lst
-
-    ### Removes unused columns from weather dataframe and converts objects to datatypes. 
-    ### Takes an optional month name string to return a dataframe for a single month  
-    def format_weather(current_df, month=""):
-        print("Formatting weather dataframe...")
-        print("Changing datatypes...")
-        new_df = current_df.astype({'STATION':'category',
-                                    'DATE':'datetime64[ns]'})
-        print("Dropping columns...")
-        new_df.drop(labels=['ELEVATION','LATITUDE', 'LONGITUDE', 'MDSF', 'AWND', 'SNWD',
-                            'WESD', 'WESF','WT01', 'WT02', 'WT03',
-                            'WT04', 'WT05',"WT06","WT07","WT08",
-                            "WT09","WT11"], axis=1, inplace=True)
-        if month == "" :
-            print("Done formatting weather dataframe")
-            return new_df
-        else:
-            print("Done formatting weather dataframe")
-            new_df_w_month = new_df.loc[new_df['DATE'].apply(pd.Timestamp.month_name) == month]
-            return new_df_w_month
     
-    ### clean up services dataframes
-    ### Drop status column bc it's not used and lateness for Amtrak is not tracked
-    ### in the CSV file. Meadowlands is a special service so that is dropped as well.
-    def format_services(current_df):
-        print("Dropping columns...")
-        if 'status' in current_df:
-            current_df.drop(labels=['status'], axis=1, inplace=True)
-        if 'line' in current_df:
-            current_df.drop(current_df[current_df['line'] == 'Meadowlands Rail'].index, inplace=True)
-        if 'type' in current_df:
-            current_df.drop(labels=['type'], inplace=True)
-        if 'stop_sequence' in current_df:
-            current_df.drop(labels=['stop_sequence'], axis=1, inplace=True)
-        if ('from_id' in current_df) & ('to_id' in current_df):
-            current_df.drop(labels=['from_id', 'to_id'], axis=1, inplace=True)
-        print("Changing datatypes...")
-        new_df = current_df.astype({'date' : 'datetime64[ns]',
-                        'train_id' : 'category',
-                        'from' : 'category',
-                        'to' : 'category',
-                        'scheduled_time' : 'datetime64[ns]',
-                        'actual_time' : 'datetime64[ns]',
-                        'delay_minutes' : 'float16',
-                        'line' : 'category'},
-                        errors='ignore')
-        new_df.dropna(how='any', inplace=True)
-        print("Done formatting dataframe")
-        return new_df
-
     ### create new dataframe and assign types
     ### this is used for breaking down performence by rail line
     def get_avg_longest(list_njt_lines, list_max_delays, list_avg_delays, list_dates):
@@ -108,49 +59,46 @@ class helper():
                             })
         return df
     
-    ### combines all CSV files into a single dataframe
-    ### it also exports the dataframe into a combined CSV file
-    ### combines all CSV files into a single dataframe
-    ### it also exports the dataframe into a combined CSV file
-    def combine_csvs(directory):
-        csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
-        dfs = []
-
-        print("Combining CSV files....")
-        for file in csv_files:
-            df = pd.read_csv(os.path.join(directory, file))
-            dfs.append(df)
-        combined_df = pd.concat(dfs, ignore_index=True)
-        print("Formatting new CSV file...")
-        combined_df = helper.format_services(combined_df)
-        # make a new CSV for the dataframe
-        print("Exporting....")
-        compression_opts = dict(method='zip', archive_name='all_services.csv')  
-        combined_df.to_csv('out.zip', index=False, compression=compression_opts)
-
-        print("CSV files successfully combined and exported.")  
-        return combined_df
-
     ### iterates through the data frame to retrieve the values of the delay_minutes column
     ### in each row. 
     ### count[0] = on time
     ### count[1] = 6-10 mins late
     ### count[2] = 10-15 mins late
     ### count[3] = >15 mins late
-    def categorize_lateness(dataframe):
+    def count_lateness(dataframe):
         count = [0, 0, 0, 0]
         column = dataframe['delay_minutes']
-        for delay in column:
-            if(delay < 6.0):
+        for time in column:
+            if(time < 6.0):
                 count[0] += 1
-            elif((delay >= 6.0) & (delay < 10.0) ):
+            elif((time >= 6.0) & (time < 10.0)):
                 count[1] += 1
-            elif((delay >= 10.0) & (delay < 15.0)):
+            elif((time >= 10.0) & (time < 15.0)):
                 count[2] += 1
             else:
                 count[3] += 1
         return count
     
+    ### Categorizes lateness by taking the 'delay_minutes' value
+    ### of a row and entering it a correspondig array.
+    ### Returns a combined array.
+    def categroize_lateness(dataframe):
+        lt_6 = []
+        six_to_ten = []
+        ten_to_fifteen = []
+        gt_15 = []
+        column = dataframe['delay_minutes']
+        for time in column:
+            if(time < 6.0):
+                lt_6.append(time)
+            elif((time >= 6.0) & (time < 10.0)):
+                six_to_ten.append(time)
+            elif((time >= 10) & (time < 15.0)):
+                ten_to_fifteen.append(time)
+            else:
+                gt_15.append(time)
+        all_arrays = np.array([lt_6, six_to_ten, ten_to_fifteen, gt_15], dtype='object')
+        return all_arrays
     ########################################
     
     def get_otp_data(dataframe,column_name, categories, year = None, month = None):
@@ -158,13 +106,13 @@ class helper():
         if((year == None) & (month == None)):
                 print("none")
                 for item in categories:
-                        otp_item = helper.categorize_lateness(dataframe[(dataframe[column_name] == item)])
+                        otp_item = helper.count_lateness(dataframe[(dataframe[column_name] == item)])
                         otp_data.append(otp_item)
                 return otp_data
         elif((year != None) & (month != None)):
                 print("year and month")
                 for item in categories:
-                        otp_item = helper.categorize_lateness((dataframe[column_name] == item) & 
+                        otp_item = helper.count_lateness((dataframe[column_name] == item) & 
                                                               (dataframe['date'].dt.year == year) & 
                                                               (dataframe['date'].dt.month == month))
                         otp_data.append(otp_item)
@@ -172,34 +120,22 @@ class helper():
         elif (year != None):
                 print("by year")
                 for item in categories:
-                        otp_item = helper.categorize_lateness(dataframe[(dataframe[column_name] == item) & 
+                        otp_item = helper.count_lateness(dataframe[(dataframe[column_name] == item) & 
                                                                            (dataframe['date'].dt.year == year)])
                         otp_data.append(otp_item)
                 return otp_data
         elif (month != None):
              print("by month")
              for item in categories:
-                        otp_item = helper.categorize_lateness(dataframe[(dataframe[column_name] == item) & 
+                        otp_item = helper.count_lateness(dataframe[(dataframe[column_name] == item) & 
                                                                            (dataframe['date'].dt.month == month)])
                         otp_data.append(otp_item)
         return np.asarray(otp_data)
-    
-    ### Takes an array of integer arrays as a parameter
-    ### and iteratively plots the data
-    def chart_subplots(data, cat_labels, leg_labels, otp_colors,title=""):
-        fig = plt.figure(figsize=(15,12))
-        for n, count in enumerate(data):
-            ax = plt.subplot(3,4, n+1)
-            ax.pie(data[n], colors=otp_colors, radius = 1.2, autopct = "%0.2f%%", startangle=270)
-            ax.title.set_text(cat_labels[n])
-        plt.legend(bbox_to_anchor = (1.5, 1.0), labels = leg_labels)
-        fig.suptitle(title,fontsize=16)
-        plt.show()
         
     ### calculate the on time performance of a dataframe
     ### OTP is calculated Services On Time divided by Total Services multipleid by 100
     def on_time_performance(dataframe):
-        count = helper.categorize_lateness(dataframe)
+        count = helper.count_lateness(dataframe)
         on_time = count[0]
         total_srvc = on_time + count[1] + count[2] + count[3]
         if total_srvc == 0:
@@ -210,7 +146,7 @@ class helper():
     ### calculate the probability of a train being late
     ### essentially the on_time_perforamnce() function upside down
     def late_prob(dataframe):
-         count = helper.categorize_lateness(dataframe)
+         count = helper.count_lateness(dataframe)
          late_trains = count[1] + count[2] + count[3]
          total_srvc = late_trains + count[0]
          if total_srvc == 0:
@@ -283,3 +219,24 @@ class helper():
              if(delay > 5.99):
                   late_arrivals.append(delay)
         return late_arrivals
+    
+    ### Prints proababilties using CDF
+    def print_prob_cdf(df, destination):
+        print("CUMULATIVE DISTRIBUTIONS FOR " + destination.upper())
+        print('-----------------------------------------------------')
+        trains_to_dest = df[(df['to'] == destination)].copy()
+        # group delay_minutes based on value
+        delay_minutes = trains_to_dest['delay_minutes']
+        count_minutes = helper.count_lateness(trains_to_dest)
+        print(count_minutes[3]/(count_minutes[0] + count_minutes[1] + count_minutes[2] + count_minutes[3]))
+        delay_minutes = np.array(trains_to_dest['delay_minutes'])
+        # Setup Cumulative Distribution Function
+        mu = np.mean(delay_minutes)
+        sigma = statistics.stdev(delay_minutes)
+        print(f'Mu: {mu}    Sigma: {sigma}')
+        print(f'P(Y < 6) = norm.cdf(6, loc={mu}, scale={sigma}) = {stats.norm.cdf(6, loc=mu, scale=sigma):.4f}')
+        print(f'P(6 < Y < 10) = stats.norm.cdf(10, loc={mu}, scale={sigma}) - stats.norm.cdf(6, loc={mu}, scale={sigma}) = ')
+        print(f'{stats.norm.cdf(10, loc=mu, scale=sigma) - stats.norm.cdf(6, loc=mu, scale=sigma):.4f}')
+        print(f'P(10 < Y < 15) = stats.norm.cdf(15, loc={mu}, scale={sigma}) - stats.norm.cdf(10, loc={mu}, scale={sigma}) = ')
+        print(f'{stats.norm.cdf(15, loc=mu, scale=sigma) - stats.norm.cdf(10, loc=mu, scale=sigma):.4f}')
+        print(f'P(Y > 15) = 1 - norm.cdf(15, loc={mu}, scale={sigma}) = {1 - stats.norm.cdf(6, loc=mu, scale=sigma):.4f}\n')
